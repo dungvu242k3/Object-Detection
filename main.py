7cf4c79325317c77fb1ba3aedf898c08b647b4d0
dungvu242k3 <90087085+dungvu242k3@users.noreply.github.com>
2025-05-30 16:39:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-import io
 import os
 from io import BytesIO
 
 import cv2
 import numpy as np
 import requests
-import yaml
+import torch
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import StreamingResponse
 from ultralytics import YOLO
 
-base_dir = "C:/Users/dungv/Object-Detection"
-class_names = ["Ambulance", "Bus", "Car", "Motorbike", "Truck"]
-
-yaml_content = {
-    "path": base_dir,
-    "train": "train/images",
-    "val": "test/images",
-    "names": class_names,
-    "nc": len(class_names)
-}
-
-yaml_path = os.path.join(base_dir, "data.yaml")
-with open(yaml_path, "w") as f:
-    yaml.dump(yaml_content, f)
-
-model = YOLO("yolo12n.pt")
-
 app = FastAPI()
-label_map = {0: 'Ambulance', 1: 'Bus', 2: 'Car', 3: 'Motorbike', 4: 'Truck'}
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "best.pt")
+
+model = YOLO(MODEL_PATH).to("cpu")
+
 
 def download_image(url: str):
     try:
@@ -37,23 +23,23 @@ def download_image(url: str):
         arr = np.asarray(bytearray(resp.content), dtype=np.uint8)
         img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
         if img is None:
-            raise ValueError("Invalid image")
+            raise ValueError("Failed to decode image")
         return img
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Failed to download image: {e}")
+        raise HTTPException(status_code=400, detail=f"Cannot download image: {e}")
 
-@app.get("/predict/")
-def predict(image_url: str = Query(...)):
+def run_inference(img):
+    results = model(img)
+    annotated_img = results[0].plot()
+    return annotated_img
+
+@app.get("/detect/")
+def detect_api(image_url: str = Query(..., description="URL của ảnh")):
     img = download_image(image_url)
-    results = model.predict(source=img, save=False, imgsz=640, conf=0.25)
-    annotated = results[0].plot()
-    _, img_encoded = cv2.imencode(".jpg", annotated)
+    annotated_img = run_inference(img)
+    _, img_encoded = cv2.imencode('.jpg', annotated_img)
     return StreamingResponse(BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
 
-
 if __name__ == "__main__":
-    model_train = YOLO("yolo12n.pt")
-    model_train.train(data=yaml_path, epochs=100, imgsz=640, batch=32)
-
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
